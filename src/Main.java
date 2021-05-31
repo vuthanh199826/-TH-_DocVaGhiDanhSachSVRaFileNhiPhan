f2407b1b7821fc20e1367399dad5f0c8b3e55bbb
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-31 18:23:07 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("name1", 19));
+        students.add(new Student("name2", 19));
+        students.add(new Student("name3", 19));
+        writeToFile("student.txt", students);
+        List<Student> studentDataFromFile = readDataFromFile("student.txt");
+        for (int i = 0; i < studentDataFromFile.size(); i++) {
+            System.out.println(studentDataFromFile.get(i));
+        }
+
+    }
+
+    public static void writeToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> readDataFromFile(String path) {
+        List<Student> students = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return students;
+    }
+}
